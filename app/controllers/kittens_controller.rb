2acfff0e01d55f81cf6869ec67a3738d25e02c6e
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-08-10 13:40:55 -0500

    Add Delete method for Kitten
    There is now an option to delete a kitten on their page.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -36,6 +36,13 @@ class KittensController < ApplicationController
         end
     end
 
+    def destroy
+        @kitten = Kitten.find(params[:id])
+        @kitten.destroy
+
+        redirect_to root_path, status: :see_other
+    end
+
     
 
     private
